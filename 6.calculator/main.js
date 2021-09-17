b9adfae49c220ed9d9900a69cd22cd8f7fea7098
Nishchala Barde <28265373+nishchala@users.noreply.github.com>
2021-09-17 12:12:21 -0400

    Calculator basic operations completed

---
+++ 6.calculator/main.js
@@ -0,0 +1,99 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const display = document.getElementById("display");
+    display.textContent = "0";
+    
+})
+
+let num1flag = 0, num2flag = 0, num1 =0, num2 =0, opflag = 0;
+let op = "";
+
+const num = document.getElementsByClassName("num");
+for(let i=0; i < num.length; i++) {
+    num[i].addEventListener("click", ()=>{
+        if(num1flag ==0 && opflag==0 && num2flag==0) {
+            num1=Number(num[i].innerHTML);
+            console.log(num1);
+            display(num1);
+            num1flag = 1;
+        }
+        else if(num1flag = 1 && opflag==0 && num2flag==0) {
+            num1 += num[i].innerHTML;
+            num1 = Number(num1);
+            console.log(num1);
+            display(num1);
+        }
+        else if(num1flag==0 && opflag==1 && num2flag==0) {
+            num2flag=1;
+            opflag=0;
+            num2=Number(num[i].innerHTML);
+            console.log(num2);
+            display(num2);
+        }
+        else if(num2flag==1 && num1flag==0 && opflag==0) {
+            num2 += num[i].innerHTML;
+            num2 = Number(num2);
+            console.log(num2);
+            display(num2);
+        }
+
+        // console.log(typeof Number(num[i].innerHTML));
+    });
+}
+
+const opr = document.getElementsByClassName("op");
+// console.log(opr);
+for(let i=0; i < opr.length; i++) {
+    opr[i].addEventListener("click", ()=>{
+        if(opflag==0 && num1flag==0 && num2flag==0) {
+            num1=0;
+            
+            op = opr[i].innerHTML;
+            
+             opflag=1;
+            console.log(op);
+            display(num1);
+        }
+        else if(opflag==0 && num1flag==1 && num2flag==0) {
+            num1flag=0;
+            opflag=1;
+            op = opr[i].innerHTML;
+            console.log(op);
+        }
+        else if(opflag==1 && num1flag==0 && num2flag==0) {
+            num1 = calc(num1,num2,op);
+            num2 = 0;
+            op = opr[i].innerHTML;
+            console.log(num1);
+            console.log(op);
+            display(num1);
+        }
+        else if (opflag==0 && num1flag==0 && num2flag==1) {
+            opflag=1;
+            num2flag=0;
+            num1 = calc(num1,num2,op);
+            num2=0;
+            op=opr[i].innerHTML;
+            
+
+            console.log(num1);
+            console.log(op);
+            display(num1);
+        }
+    });}
+
+    const calc = (num1,num2,op) => {
+        if(op=="+") return num1+num2;
+        if(op=="-") return num1-num2;
+        if(op=="*") return num1*num2;
+        if(op=="/") return num1/num2;
+        if(op=="%") return num1%num2;
+        if(op=="=") return num2; //to do num1 + num2 = (result) + num1
+    }
+
+    const display = (num) => {
+        const display = document.getElementById("display");
+    display.textContent = num;
+    }
+
+
+
